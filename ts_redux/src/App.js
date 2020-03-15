582a9be87027f2e121d74469ced63aa0434e188d
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-03-15 10:06:22 +0100

    Created store and rootReducer

--- ts_redux/src/App.js
+++ ts_redux/src/App.js
@@ -2,14 +2,20 @@ import React from 'react';
 import './App.css';
 import Post from './Components/Post';
 import PostForm from './Components/PostForm';
+import {Provider} from 'react-redux'
+import store from './store'
+
+// const store = createStore(() => [], {}, applyMiddleware())
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
     <PostForm/>
     <hr/>
       <Post/>
     </div>
+    </Provider>
   );
 }
 
