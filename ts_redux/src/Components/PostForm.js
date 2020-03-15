9918a3cdb040bf3304901e17c01f1fc3626939f3
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-03-15 08:58:32 +0100

    Added onSubmit method

--- ts_redux/src/Components/PostForm.js
+++ ts_redux/src/Components/PostForm.js
@@ -9,17 +9,37 @@ import React, { Component } from 'react'
         }
 
         this.handleChange = this.handleChange.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
      }
 
      handleChange(e){
      this.setState({[e.target.name] : e.target.value})
      }
 
+     onSubmit(e){
+         e.preventDefault()
+         const post = {
+         title: this.state.title,
+         body: this.state.body
+         }
+
+         fetch('https://jsonplaceholder.typicode.com/posts', {
+             method: 'POST',
+             headers:{
+                 'content-type': 'application/json'
+             },
+             body: JSON.stringify(post)
+         })
+         .then(res => res.json())
+         .then(res => console.log(res)
+         )
+     }
+
     render() {
         return (
             <div>
                <h1>Add Post</h1> 
-               <form>
+               <form onSubmit={this.onSubmit}>
                <div>
                <label>Title</label> <br/>
                <input 
