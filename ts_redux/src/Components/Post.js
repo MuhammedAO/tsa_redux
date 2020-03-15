d23041fec3d917fae1b5b72a095b27a47ec9639c
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-03-15 21:16:08 +0100

    Clean up post.js for postActions.js

--- ts_redux/src/Components/Post.js
+++ ts_redux/src/Components/Post.js
@@ -10,12 +10,7 @@ import React, { Component } from 'react'
      }
     }
 
-    componentDidMount(){
-        fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(res => res.json())
-        .then(res => this.setState({posts:res}))
-    }
-
+   
     render() {
         const postItems = this.state.posts.map(post => (
          <div key={post.id}>
